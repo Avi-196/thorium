9d73307b9cafaef3a27c72806f24827495a29fc9
Avi-196 <avinaymishra63@gmail.com>
2022-02-23 03:17:08 +0530

    23feb assignment.api

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,6 +7,50 @@ router.get('/students/:name', function(req, res) {
     res.send(studentName)
 })
 
+//1. get/movies return the array  movies
+router.get('/movies', function(req,res) {
+    res.send('["pushpa","udaan","venom","matrix","endgame"]')
+
+})
+//2 GET /movies/1 is a valid request and it should return the movie in your array at index 1
+
+router.get('/movies/:movieId',function(req,res) {
+     let movi=["pushpa","udaan","venom","matrix","endgame"]
+    let value=req.params.movieId;
+    if (value>movi.lenghth-1){
+        res.send("does not exist")
+    }else{
+        res.send(movi[value])
+    }
+})
+ //3
+ //router.get('/moviezs',function(req,res){
+   //  res.send()
+ //}
+
+ //4
+   router.get('/films/:filmId',function(req,res){
+       let moviee=[{id:1,name:'The shining'},{id:2,name:'incendies'},{id:3,name:"Rang De Basanti"},{id:4,name:"Finding Demo"}]
+       let value=res.params.filmId;
+       let found =false;
+       for(i=0;i<moviee.length;i++){
+           if (moviee[i].id==value){
+               found=true
+               res.send(moviee[i])
+               break;
+
+           }
+       } 
+       if(found==false){
+           res.send('No movies exists with this id')
+       }
+       
+   })
+
+
+
+
+
 
 
 
